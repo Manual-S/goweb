e70ccb8b415c1972714fbb922495a5a56cb6087b
GG Bond <1075866471@qq.com>
2022-06-12 10:44:40 +0800

    ADD:实现服务优雅关闭

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"goweb/framework"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -12,5 +17,17 @@ func main() {
 		Handler: core,
 		Addr:    ":8080",
 	}
-	ser.ListenAndServe()
+	go func() {
+		ser.ListenAndServe()
+	}()
+
+	// 监听进程终止信号
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 没有信号到来 主goroutine阻塞
+	<-quit
+
+	if err := ser.Shutdown(context.Background()); err != nil {
+		log.Fatalf("server Shutdown error %v", err)
+	}
 }
